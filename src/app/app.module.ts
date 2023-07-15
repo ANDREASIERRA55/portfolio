0893ea5059a0c1c6d268c8938a70ae35fe570379
Andrea103 <andreasierra103@gmail.com>
2023-07-15 19:08:21 +0200

    Nuevo proyecto componentes header home about contact work imagen de fondo tipografia y routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { WorkComponent } from './pages/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
